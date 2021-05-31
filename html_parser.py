493a064420661205a0eef0e8ba2214ba563a8b71
Ryan Hartman <>
2021-05-31 02:38:47 +0000

    added task retrieval functionality and beginning stages for chatbot functions

--- html_parser.py
+++ html_parser.py
@@ -5,3 +5,28 @@ from urllib.request import urlopen
 
 # global variable creation 
 url = 'https://1729.com/all/'
+
+# Parsing html into BS object
+page = urlopen(url)
+html = page.read().decode('utf-8')
+soup = BeautifulSoup(html,'html.parser')
+
+# Checking soup
+text = soup.get_text()
+
+# Creating first_task
+def Task1():
+    First_Earn = text.find('Earn',text.find('Earn',0)+5)
+
+    Second_Earn = text.find('Earn',First_Earn)
+
+    first_task = text[Second_Earn:text.find('read',Second_Earn)+4]
+
+    first_task = first_task[4:].lstrip()
+
+    return first_task
+
+
+
+
+
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 import json
 import random
 from replit import db
+from html_parser import Task1
 from keep_alive import keep_alive
 
 # Establishing client
@@ -59,7 +60,8 @@ async def on_message(message):
     
     msg = message.content
 
-    if message.content.startswith('$inspire'):
+    # Responds with random inspirational quote from database
+    if message.content.startswith('!inspire'):
         quote = get_quote()
         await message.channel.send(quote)
     
@@ -73,22 +75,25 @@ async def on_message(message):
         if any(word in msg for word in sad_words):
             await message.channel.send(random.choice(options))
 
-    if msg.startswith('$new'):
-        encouraging_message = msg.split("$new ",1)[1]
+    # Adds new encouragements to database
+    if msg.startswith('!new'):
+        encouraging_message = msg.split("!new ",1)[1]
         update_encouragements(encouraging_message)
         await message.channel.send('Your encouraging message has been added')
     
-    if msg.startswith('$del'):
+    # deletes encouragemnets based off of indices within database
+    if msg.startswith('!del'):
         encouragements = []
         if "encouragements" in db.keys():
-            index = int(msg.split('$del',1)[1])
+            index = int(msg.split('!del',1)[1])
             delete_encouragement(index)
             encouragements = db['encouragements']
             await message.channel.send(encouragements)
         else: 
             await message.channel.send('There is nothing to delete!')
 
-    if msg.startswith('$list'):
+    # List of encouragements available in database
+    if msg.startswith('!list'):
         encouragements = []
         if 'encouragements' in db.keys():
             encouragements = db['encouragements']
@@ -96,8 +101,9 @@ async def on_message(message):
         else: 
             await message.channel.send('The list is empty!')
     
-    if msg.startswith('$responding'):
-        command = msg.split('$responding ',1)[1]
+    # Turns the bot on and off
+    if msg.startswith('!responding'):
+        command = msg.split('!responding ',1)[1]
 
         if command.lower() == 'true' or command.lower() == 'on':
             db['responding'] = True
@@ -107,7 +113,21 @@ async def on_message(message):
             await message.channel.send('Responding turned off!')
         else: 
             await message.channel.send('Invalid command! Options: true, false')
-        
+    # Responds with newest 'earn' task available on 1729.com        
+    if msg.startswith('!Newest Task'):
+        await message.channel.send(Task1())
+
+    # Responds with description of bot
+    if msg.startswith('!Who are you?'):
+        await message.channel.send('Hello! I am a bot created to give a brief summary of tasks available on 1729.com where you can earn Bitcoin for performing certain actions in the world around us! /n If you would like to learn more about me say "!README" to get a link to my README.md file on GitHub!')
+    
+    # Readme message
+    if msg.startswith('!README'):
+        await message.channel.send('https://github.com/ryanhartman4/1729_Discord_Bot/blob/main/README.md')
+
+    # Chatbot response 
+    ''' in development phase '''
+
 # Running bot
 keep_alive()
 TOKEN = os.environ['token']
